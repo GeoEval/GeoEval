992fd2f348b947dafa45510559cb67b8912aa693
zzli2022 <lizhongzhi2022@ia.ac.cn>
2023-11-24 14:51:59 +0800

    <feat>(<startup>) add Param Start up .sh for Geometry3K and PGPS9K

---
+++ sh_files/geometry3k/caculate_score.sh
@@ -0,0 +1,4 @@
+python3 tool\caculate_score.py \
+    --output_dir result/gpt35 \
+    --dataset_name Geometry3K \
+    --problem_file test.json 
---
+++ sh_files/geometry3k/get_response.sh
@@ -0,0 +1,3 @@
+python3 icl_gpt_run.py \
+    --dataset Geometry3K \
+    --model_name gpt35 
