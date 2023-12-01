b0beaec95c0f20fb8628a71587ac2086edad44e6
KnightZhang <knight.625@live.com>
2023-12-01 14:42:22 +0000

    :sparkles: optimize the code: 1. move all configuration to the 'sh_files/eval_config.yaml" 2. execute from the "sh_files/unigeo/evaluate_unigeo.sh" for example 3. change all path to the flexible absolute path (change accordingly) 4. save files fomr "LLM_eval" to the Dataset generator, and can save as cache.

--- run_llm_response.py
+++ run_llm_response.py
@@ -3,37 +3,55 @@ import json
 from tool.gpt_tool import get_chat_reponse
 from tool.opensource_llm_tool import get_opensource_llm_reponse
 from tool.util import read_json, save_json
-from model import create_model_with_tokenizer
+from model import create_model_tokenizer
 from tqdm import tqdm
 import argparse 
 import os
+import datasets
+from tool import read_yaml
+from pathlib import Path
+from argparse import Namespace
+from datetime import datetime
+from datasets import Dataset
+from torch.utils.data import DataLoader
+MAIN_PATH = Path(__file__).absolute().parent
 
-
+from tool.tokenized_data import read_jsonl, collate_fn
 
 dataset_list = ['Geometry3K','PGPS9K', 'UniGeo_Cat', 'UniGeo_Prove']
 type_list = ['api_model', 'opensource_model']
 model_list = ['gpt35', '']
 
 def eval(args, dataset_path, output_json_file):
-    data_load = read_json(dataset_path)
-
     if args.model_type == 'api_model':
         eval_api_model(dataset_path, output_json_file)
     if args.model_type == 'opensource_model':
         eval_opensource_model(args, dataset_path, output_json_file)
 
 def eval_opensource_model(args, dataset_path, output_json_file):
+    # create model, tokenizer
+    model, tokenizer = create_model_tokenizer(args, from_local=args.from_local)    
     
-    data_load = read_json(dataset_path)
-    answer_dict = {}
-    model, tokenizer = create_model_with_tokenizer(args)
+    # create or load cached data
+    if not args.cached_file:
+        dataset = Dataset.from_generator(
+            lambda: read_jsonl(
+                path=dataset_path,
+                max_seq_length=args.max_seq_length,
+                tokenizer=tokenizer,
+                args=args,
+            )
+        )
+        dataset.save_to_disk(os.path.join(args.cached_dir, f"{args.dataset}_{args.model_name}"))
+    else:
+        assert os.path.exists(args.cached_file), f"cached processed data doesn't exist: {args.cached_file}"
+        dataset = datasets.load_from_disk(args.cached_file)
     
-    for data_k, question in tqdm(data_load.items()):
-        answer_result = get_opensource_llm_reponse(question, model, tokenizer)
-        answer_dict[data_k] =  answer_result
+    data_loader = DataLoader(dataset, batch_size=args.bsz, collate_fn=collate_fn)
+        
+    answer_result = get_opensource_llm_reponse(data_loader, model, tokenizer)
 
-    save_json(answer_dict, output_json_file)
-    return model, tokenizer
+    save_json(answer_result, output_json_file)
 
 def eval_api_model(dataset_path, output_json_file):
     data_load = read_json(dataset_path)
@@ -47,20 +65,21 @@ def eval_api_model(dataset_path, output_json_file):
         
 if __name__ == '__main__':
     parser = argparse.ArgumentParser()
-    # input
-    parser.add_argument('--dataset', type=str, choices=dataset_list, default='PGPS9K')
-    parser.add_argument('--model_type', type=str, choices=type_list, default='api_model')
-    parser.add_argument('--model_name', type=str, choices=model_list, default='gpt35', default='gpt35')
-    parser.add_argument('--eval_dataset_path', type=str, default='LLM_eval/')
-    parser.add_argument('--input_file', type=str, default='eval_{dataset}_test.json')
-    parser.add_argument('--output_dir', type=str, default='{model_name}')
-    parser.add_argument('--output_file', type=str, default='eval_{dataset}_result.json')
-    # for model_path input
-    parser.add_argument('--weight_path', type=str, default='./')
+    parser.add_argument("--config_yaml", type=str)
     args = parser.parse_args()
+    config = read_yaml(os.path.join(MAIN_PATH, args.config_yaml))
     
-    dataset_path = os.path.join(args.eval_dataset_path, args.input_file.format_map(dict(dataset=args.dataset)))
-    output_json_dir = args.output_dir.format_map(dict(model_name=args.model_name))
-    output_json_file = args.output_file.format_map(dict(dataset=args.dataset))
-    output_json_file = os.path.join('./result', output_json_dir, output_json_file)
+    args = vars(args)
+    for config_name, configs in config.items():
+        if config_name == "General":
+            for key, val in configs.items():
+                args[key] = val
+    args = Namespace(**args)
+
+    dataset_path = os.path.join(MAIN_PATH, args.eval_dataset_path, args.input_file)
+    output_json_dir = os.path.join(MAIN_PATH, args.output_dir)
+    now_time = datetime.now().strftime("%Y-%m-%d-%H-%M-%S")
+    output_json_file = os.path.join(output_json_dir, f"{args.dataset}_{args.model_name}_{now_time}.json")
+    args.cached_dir = os.path.join(MAIN_PATH, args.cached_dir)
+    
     eval(args, dataset_path, output_json_file)
