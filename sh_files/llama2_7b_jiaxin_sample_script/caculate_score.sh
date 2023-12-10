0b22875d18204d354335fbc6b7c9781cfcca6c31
Jiaxin Zhang <gvb20207@archie-w.hpc.strath.ac.uk>
2023-12-10 20:04:52 +0000

    :art: create sample script

---
+++ sh_files/llama2_7b_jiaxin_sample_script/caculate_score.sh
@@ -0,0 +1,4 @@
+python3 tool\caculate_score.py \
+    --output_dir result/gpt35 \
+    --dataset_name UniGeo_Cat \
+    --problem_file test.json 
---
+++ sh_files/llama2_7b_jiaxin_sample_script/evaluate.sh
@@ -0,0 +1,22 @@
+#!/usr/bin/env bash
+
+# set -xe
+
+MAIN_DIR=../..
+YOUR_CREATE_DIR=llama2_7b_jiaxin_sample_script
+CONFIG_YAML="sh_files/${YOUR_CREATE_DIR}/eval_config.yaml"
+
+ARGS="--config_yaml ${CONFIG_YAML}"
+
+
+# sh_files/llama2_7b_jiaxin_sample_script/../../run_llm_response.py",
+# cpu
+python ${MAIN_DIR}/run_llm_response.py ${ARGS}
+
+# # single gpu
+# CUDA_VISIBLE_DEVICES=0 python main.py ${ARGS}
+
+# two gpus on one node
+# torchrun --nproc_per_node=2 main.py ${ARGS}
+
+# python -m torch.distributed.launch --nproc_per_node=2 --use_env main.py ${ARGS}
