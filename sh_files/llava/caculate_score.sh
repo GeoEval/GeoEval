4b81df811bec352f23d53526d2ac09c171e81df6
zzli2022 <lizhongzhi2022@ia.ac.cn>
2024-01-07 12:37:37 +0800

    fix llava sh_files: model name

--- sh_files/llava/caculate_score.sh
+++ sh_files/llava/caculate_score.sh
@@ -1,4 +1,4 @@
 python3 tool/caculate_score.py \
-    --model_name mplug_owl2 \
+    --model_name llava \
     --dataset_name GeoEval \
     --problem_file select.json 
--- sh_files/llava/ext_result.sh
+++ sh_files/llava/ext_result.sh
@@ -1,3 +1,3 @@
 python3 tool/extract_result.py \
-    --model_name mplug_owl2 \
+    --model_name llava \
     --quick_extract 
