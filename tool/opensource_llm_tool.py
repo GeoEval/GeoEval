6d513b064f6f1c54e7a1aee1d4d42aab32ec1103
KnightZhang <knight.625@live.com>
2024-01-07 11:58:01 +0000

    :bug: use unique_id

--- tool/opensource_llm_tool.py
+++ tool/opensource_llm_tool.py
@@ -2,9 +2,7 @@ import torch
 from tqdm import tqdm
 device = "cuda" if torch.cuda.is_available() else "cpu"
 
-
-
-def get_opensource_llm_reponse(data_loader, model, tokenizer, sample_number):
+def get_opensource_llm_reponse(data_loader, model, tokenizer, sample_number, max_seq_length):
     predictions = {}
     count_number = sample_number
     for feature in tqdm(data_loader):
@@ -15,21 +13,22 @@ def get_opensource_llm_reponse(data_loader, model, tokenizer, sample_number):
         assert inputs.size(0) == 1, print(f"Only support batch size equal to, however, get {inputs.size(0)}")
         o_len = feature["input_len"][0]
         pred = model.generate(inputs,
-                        max_length=1024,
+                        max_length=max_seq_length,
                         do_sample=True,
                         top_k=50,
                         top_p=0.5,
                         num_return_sequences=1,
                         pad_token_id=tokenizer.eos_token_id)
         return_text = tokenizer.decode(pred.cpu()[0], skip_special_tokens=True)[o_len:]
-        # return_text_all = tokenizer.decode(pred.cpu()[0], skip_special_tokens=True)
+        return_text_all = tokenizer.decode(pred.cpu()[0], skip_special_tokens=True)
 
         meta_data = feature["meta_data"][0]
-        data_id = meta_data["dataset_id"]
+        data_id = meta_data["unique_id"]
 
         predictions[data_id] = {
                 "Problem": meta_data["sentence"],
                 "Response": return_text,
+                "Response_all": return_text_all,
                 "answer_value": meta_data["answer_value"],
                 "choice_list": meta_data["choice_list"],
                 "choice_id": meta_data["choice_id"],
@@ -38,6 +37,6 @@ def get_opensource_llm_reponse(data_loader, model, tokenizer, sample_number):
         for key, val in meta_data.items():
             if key not in predictions[data_id]:
                 predictions[data_id][key] = val
-                
+
         count_number -=1
     return predictions
