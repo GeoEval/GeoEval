78337a2630a8c506d80abb9b340a370874f92033
zzli2022 <lizhongzhi2022@ia.ac.cn>
2024-02-14 22:59:52 +0800

    merge_eval_config

--- sh_files/gpt35/evaluate_general.sh
+++ sh_files/gpt35/evaluate_general.sh
@@ -1,16 +1,4 @@
 CONFIG_YAML="sh_files/gpt35/eval_config.yaml"
 
-<<<<<<< HEAD
-# ARGS="--config_yaml ${CONFIG_YAML} --prompt_type general"
-# python3 run_llm_response.py ${ARGS}
-
-# ARGS="--config_yaml ${CONFIG_YAML} --prompt_type general --aug_reason --output_type aug_result"
-# python3 run_llm_response.py ${ARGS}
-
-# ARGS="--config_yaml ${CONFIG_YAML} --prompt_type general --backward_reason --output_type backaug_result"
-# python3 run_llm_response.py ${ARGS}
-
-=======
 ARGS="--config_yaml ${CONFIG_YAML} --prompt_type general"
 python3 run_llm_response.py ${ARGS}
->>>>>>> a8f2a3d230bcc3fa5f6b27c1703b0978584fafe2
--- sh_files/gpt35/ext_result.sh
+++ sh_files/gpt35/ext_result.sh
@@ -1,8 +1,4 @@
 # MAIN_DIR=../../
 python3 tool/extract_result.py \
-<<<<<<< HEAD
     --model_name gpt35 \
-=======
-    --model_name gpt35-turbo \
->>>>>>> a8f2a3d230bcc3fa5f6b27c1703b0978584fafe2
     --quick_extract 
